063d935181d24e738df8004aa14b138edd85adb1
md_mumtaj_alam <mmumtajalam987@gmail.com>
2022-08-28 11:26:17 +0530

    api

--- router/orders.js
+++ router/orders.js
@@ -5,10 +5,10 @@ const Orders = require("../models/Orders");
 const router = express.Router();
 
 //http://localhost:4000/orders/placeorder
-router.post("/", async (req, res) => {
+router.post("/placeorder", async (req, res) => {
   try {
     const tempOrder = new Orders({
-      orderid: parseInt(Math.random() * 1000000),
+      orderid: parseInt(Math.random() * 10000000000),
       rest_id: req.body.rest_id,
       rest_name: req.body.rest_name,
       city: req.body.city,
@@ -20,6 +20,34 @@ router.post("/", async (req, res) => {
     res.status(400).json(err);
   }
 });
+
+// //http://localhost:4000/orders/placeorder
+// router.post("/placeorder", verifyJwt, async (req, res) => {
+//   //console.log(req.headers["x-access-token"]);
+//   try {
+//     const tempOrder = new Orders({
+//       orderid: parseInt(Math.random() * 10000000000),
+//       username: req.body.username,
+//       rest_id: req.body.rest_id,
+//       rest_name: req.body.rest_name,
+//       city: req.body.city,
+//       amount: req.body.amount,
+//     });
+//     const response = await tempOrder.save();
+//     //store order details in loop - req.body.foodItems
+//     // const tempOrder = new OrderDetails({
+//     //     orderid: response.orderid,
+//     //     food_id: req.body,
+//     //     food_name: req.body,
+//     //     quantity: req.body,
+//     //     price: req.body
+//     // })
+//     // const response = await tempOrder.save();
+//     res.status(201).json(response);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
 
 //http://localhost:4000/orders/allorders
 router.get("/allorders", async (req, res) => {
